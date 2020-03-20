caa8d658bb3eb24458b5f6fa37ae0f3e6ae6fab9
Kingsley Victor <javaprodigy56@gmail.com>
2020-03-20 19:17:55 +0100

    Wire probe endpoint for KintoHub's healthcheck

--- src/config/index.js
+++ src/config/index.js
@@ -4,13 +4,15 @@ const APIRouter = require("../router");
 module.exports = (app) => {
   return (logger, { json, urlencoded }) => {
     app.use(json());
-    app.use(urlencoded())
+    app.use(urlencoded({
+      extended: false
+    }))
     app.use(logger("dev"));
     app.get("/", (req, res) => {
       res.status(200).json({
         body: {
           message: "This is probe endpoint for KintoHub's healthcheck.",
-          req
+          url: req.url
         }
       });
     });
