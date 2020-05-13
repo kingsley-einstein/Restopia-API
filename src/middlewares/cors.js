c765e93fa466ac03d2dda2b9e7afea04de9fb22d
Kingsley Victor <javaprodigy56@gmail.com>
2020-05-13 17:07:33 +0100

    new commit

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -2,6 +2,7 @@ module.exports = (origin, headers) => {
   return (req, res, next) => {
     res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Headers", headers || "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT");
     next();
   };
 }
